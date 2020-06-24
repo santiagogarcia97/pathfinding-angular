1eed4933259174d4616471a12f965f8e1960548e
Santiago Garcia <santiagogarcia97@outlook.com>
2020-06-23 23:18:12 -0300

    added wighted grid visualization

--- src/app/pathfinding/dijkstra.ts
+++ src/app/pathfinding/dijkstra.ts
@@ -59,21 +59,23 @@ const updateUnvisitedNeighbours = (node: Node, grid: Node[][]): void => {
   const unvisitedNeighbours: Node[] = [];
   const { col, row } = node;
 
-  if (col > 0 && !grid[row][col - 1].visited )
+  if (col > 0)// && !grid[row][col - 1].visited )
     { unvisitedNeighbours.push(grid[row][col - 1]); }
 
-  if (row > 0 && !grid[row - 1][col].visited)
+  if (row > 0)// && !grid[row - 1][col].visited)
     { unvisitedNeighbours.push(grid[row - 1][col]); }
 
-  if (col < grid[0].length - 1 && !grid[row][col + 1].visited)
+  if (col < grid[0].length - 1)// && !grid[row][col + 1].visited)
     { unvisitedNeighbours.push(grid[row][col + 1]); }
 
-  if (row < grid.length - 1 && !grid[row + 1][col].visited)
+  if (row < grid.length - 1)// && !grid[row + 1][col].visited)
     { unvisitedNeighbours.push(grid[row + 1][col]); }
 
   for (const neighbour of unvisitedNeighbours) {
-    neighbour.distance = node.distance + neighbour.weight;
-    neighbour.previousNode = node;
+    if (neighbour.distance > node.distance + neighbour.weight) {
+      neighbour.distance = node.distance + neighbour.weight;
+      neighbour.previousNode = node;
+    }
   }
 };
 
--- src/app/pathfinding/grid/grid.component.ts
+++ src/app/pathfinding/grid/grid.component.ts
@@ -24,23 +24,33 @@ export class GridComponent implements OnInit {
   getClass(node: Node): string {
     switch (node.animation) {
       case Animation.Start:
-        return 'inner node-start';
-        break;
+        return 'inner node-start'; break;
       case Animation.End:
-        return 'inner node-end';
-        break;
+        return 'inner node-end'; break;
       case Animation.Visited:
-        return 'inner node-visited';
-        break;
+        return 'inner node-visited'; break;
       case Animation.Path:
-        return 'inner node-path';
-        break;
+        return 'inner node-path'; break;
       case Animation.Wall:
-        return 'inner node-wall';
-        break;
+        return 'inner node-wall'; break;
       case Animation.Clear:
       default:
         return 'inner';
+    }
+  }
+  getBackground(node: Node): any {
+    switch (node.weight) {
+      case 5:
+        return {'background-color': '#C3C3C3'}; break;
+      case 4:
+        return {'background-color': '#D2D2D2'}; break;
+      case 3:
+        return {'background-color': '#E1E1E1'}; break;
+      case 2:
+        return {'background-color': '#f0f0f0'}; break;
+      case 1:
+      default:
+        return {'background-color': '#ffffff'};
     }
   }
 
